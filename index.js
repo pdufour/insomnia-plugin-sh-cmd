d2015500ad1bf06aabf515268e4e48d107a0c2b6
Paul Dufour <pdufour@users.noreply.github.com>
2023-03-02 16:28:21 +0000

    Code cleanup

--- index.js
+++ index.js
@@ -3,67 +3,73 @@ const exec = promisify(require('child_process').exec);
 
 const REFRESH_CACHE_TIMEOUT = 10;
 
-const execCmd = async (cmd)  => {
+const execCmd = async (cmd, type) => {
+    try {
+        var resp = await exec(cmd);
+    } catch (failed) {
+        throw new Error('Command execution failed with ' + failed)
+    };
 
-  try {
-    var resp = await exec(cmd);
-  } catch (failed) {
-    console.log('Command execution failed with ' + failed)
-    return failed.stderr;
-  };
+    if (type == 'json') {
+        return JSON.parse(resp.stdout.trim()) // add test case
+    }
 
-  if (type == 'json') {
-    return JSON.parse(resp.stdout.trim()) // add test case
-  }
-
-  return resp.stdout.trim();
+    return resp.stdout.trim();
 };
 
 module.exports.templateTags = [
-  {
-    displayName: '/bin/sh',
-    name: 'shCmdExec',
-    description: 'Executes given command using child_process package, basically /bin/sh cmd',
-    args: [
-      {
-        displayName: 'Type',
-        type: 'enum',
-        options: [
-          { displayName: 'Execute command returning String value', value: 'string' },
-          { displayName: 'Execute command returning JSON value', value: 'json' }
+    {
+        displayName: '/bin/sh',
+        name: 'shell',
+        description: 'Executes given command using child_process package, basically /bin/sh cmd',
+        args: [
+            {
+                displayName: 'Type',
+                type: 'enum',
+                options: [
+                    { displayName: 'Execute command returning String value', value: 'string' },
+                    { displayName: 'Execute command returning JSON value', value: 'json' }
+                ],
+            },
+            {
+                displayName: 'Command/shell to be executed',
+                help: 'Executes the given command using require("child_process").exec command',
+                type: 'string',
+                encoding: 'base64',
+            },
+            {
+                displayName: 'Cache results (fresh results are still fetched in the background)',
+                type: 'boolean',
+                options: [
+                    { displayName: 'true', value: true },
+                    { displayName: 'false', value: false }
+                ],
+            },
         ],
-      },
-      {
-        displayName: 'Cache results',
-        type: 'boolean',
-        options: [
-          { displayName: 'Enable caching of results (fresh results are still fetched in the background)', value: true },
-          { displayName: 'Don\'t enable caching of results', value: false }
-        ],
-      },
-      {
-        displayName: 'Command/shell to be executed',
-        help: 'Executes the given command using require("child_process").exec command',
-        type: 'string',
-      }
-    ],
-    async run(context, type = 'string', cmd) {
-      const cacheKey = `cmd-${cmd}`;
-      const hasCachedValue = await context.store.hasItem(cacheKey);
+        async run(context, type = 'string', cmd, cacheResults) {
+            const cacheKey = `cmd-${cmd}`;
 
-      if (hasCachedValue) {
-        const cachedValue = await context.store.getItem();
+            if (cacheResults && (await context.store.hasItem(cacheKey))) {
+                const cachedValue = await context.store.getItem(cacheKey);
 
-        // refresh cache in background
-        setTimeout(async () => {
-          const cacheValue = await execCmd(key);
-          await context.store.setItem(cacheKey, cacheValue);
-        }, REFRESH_CACHE_TIMEOUT);
+                // refresh cache in background
+                setTimeout(async () => {
+                    const newCachedValue = await execCmd(cmd, type);
+                    await context.store.setItem(cacheKey, newCachedValue);
+                }, REFRESH_CACHE_TIMEOUT);
 
-        return cachedValue;
-      }
+                return cachedValue;
+            } else if (!cacheResults) {
+                // purge old cache in background
+                setTimeout(async () => {
+                    context.store.removeItem(cacheKey);
+                }, REFRESH_CACHE_TIMEOUT);
+            }
 
-      return execCmd(cmd);
+            const out = await execCmd(cmd, type);
+            context.store.setItem(cacheKey, out);
+
+            return out;
+        },
     },
-  },
 ];
